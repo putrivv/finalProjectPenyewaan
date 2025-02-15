654dd0e9613ea9aa4ef5bd6c9b48441f48ffa849
dominn0 <136346563+dominn0@users.noreply.github.com>
2025-02-15 17:53:10 +0700

    daisyui

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div className="bg-red-500 text-white p-10 text-center">
+      <h1 className="text-3xl font-bold">Test Tailwind</h1>
+    </div>
+  );
+};
+
+export default login;
