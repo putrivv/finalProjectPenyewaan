62a48fc845f30fa145b16dbc8ce9b0032fa35a3c
dominn0 <136346563+dominn0@users.noreply.github.com>
2025-02-17 10:11:20 +0700

    reacticon dan sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import {
+  FaHome,
+  FaList,
+  FaTools,
+  FaShoppingCart,
+  FaUsers,
+} from "react-icons/fa";
 
 const Sidebar = () => {
   return (
     <div className="h-screen w-64 bg-[#050315] p-4 shadow-lg flex flex-col border-t-4 border-[#4A628A]">
       {/* Header */}
       <h1 className="text-xl font-bold mb-6 text-[#DFF2EB] text-center">
-        Admin
+        ADMIN
       </h1>
       <hr className="border-t-2 border-[#4A628A] mb-4" />
 
@@ -13,33 +20,47 @@ const Sidebar = () => {
       <ul className="menu space-y-3 flex-1">
         <li>
           <Link
-            href="/Admin/EditDeleteBarang"
-            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
+            href="/Admin/Home"
+            className="flex items-center py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
           >
-            Edit/Delete Barang
+            <FaHome className="mr-2" />
+            Home
+          </Link>
+        </li>
+        <hr className="border-t-2 border-[#4A628A] mb-4" />
+        <li>
+          <Link
+            href="/Admin/Kategori"
+            className="flex items-center py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
+          >
+            <FaList className="mr-2" />
+            Kategori
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/Admin/ListAlat"
+            className="flex items-center py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
+          >
+            <FaTools className="mr-2" />
+            Daftar Alat
           </Link>
         </li>
         <li>
           <Link
             href="/Admin/SewaAlat"
-            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
+            className="flex items-center py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
           >
+            <FaShoppingCart className="mr-2" />
             Penyewaan
           </Link>
         </li>
         <li>
           <Link
-            href="/Admin/ListAlat"
-            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
+            href="/Admin/Pelanggan"
+            className="flex items-center py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
           >
-            Daftar Barang
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/Admin/ListAlat"
-            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
-          >
+            <FaUsers className="mr-2" />
             Pelanggan
           </Link>
         </li>
