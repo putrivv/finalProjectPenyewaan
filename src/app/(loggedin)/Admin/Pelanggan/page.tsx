35d0d0594d8c546ef62f62bd6a095acca5e93ef9
dominn0 <136346563+dominn0@users.noreply.github.com>
2025-02-17 07:30:20 +0700

    sidebar

---
+++ src/app/(loggedin)/Admin/Pelanggan/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pelanggan = () => {
+  return (
+    <div>Pelanggan</div>
+  )
+}
+
+export default Pelanggan
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,16 +2,19 @@ import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-64 bg-[#DFF2EB] p-4 shadow-lg flex flex-col border-t-4 border-[#4A628A]">
-      <h1 className="text-xl font-bold mb-6 text-[#050315] text-center">
+    <div className="h-screen w-64 bg-[#050315] p-4 shadow-lg flex flex-col border-t-4 border-[#4A628A]">
+      {/* Header */}
+      <h1 className="text-xl font-bold mb-6 text-[#DFF2EB] text-center">
         Admin
       </h1>
       <hr className="border-t-2 border-[#4A628A] mb-4" />
+
+      {/* Menu List */}
       <ul className="menu space-y-3 flex-1">
         <li>
           <Link
             href="/Admin/EditDeleteBarang"
-            className="block py-4 px-4 rounded-lg text-[#050315] hover:bg-[#B9E5E8] transition"
+            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
           >
             Edit/Delete Barang
           </Link>
@@ -19,7 +22,7 @@ const Sidebar = () => {
         <li>
           <Link
             href="/Admin/SewaAlat"
-            className="block py-4 px-4 rounded-lg text-[#050315] hover:bg-[#B9E5E8] transition"
+            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
           >
             Penyewaan
           </Link>
@@ -27,9 +30,17 @@ const Sidebar = () => {
         <li>
           <Link
             href="/Admin/ListAlat"
-            className="block py-4 px-4 rounded-lg text-[#050315] hover:bg-[#B9E5E8] transition"
+            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
           >
             Daftar Barang
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/Admin/ListAlat"
+            className="block py-4 px-4 rounded-lg text-[#DFF2EB] hover:bg-[#B9E5E8] hover:text-[#050315] transition"
+          >
+            Pelanggan
           </Link>
         </li>
       </ul>
