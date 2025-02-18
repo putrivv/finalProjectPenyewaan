3ca44d0b4681ee672bb35d14d4c9b5f1670af225
Putri Velani <putrivelani228@gmail.com>
2025-02-18 08:51:43 +0700

    update

--- src/app/(guest)/guest/page.tsx
+++ src/app/(guest)/guest/page.tsx
@@ -6,29 +6,31 @@ const Guest = () => {
     <div className="min-h-screen flex flex-col bg-[#DFF2EB]">
       <main className="flex-1 flex items-center justify-center h-screen">
         <div className="container mx-auto flex flex-col-reverse md:flex-row items-center px-6 md:px-12">
-          <div className="pl-32 mt-0 mb-32 md:w-1/2 text-center md:text-left">
+          {/* Teks Section */}
+          <div className="md:w-1/2 text-right md:text-left">
             <h1 className="text-6xl font-bold text-gray-800">
               Selamat Datang{" "}
               <span className="text-teal-600">
                 <br />
-                di Sewa
-              </span>{" "}
-              Barang
+                di Sewa Barang
+              </span>
             </h1>
             <p className="mt-4 text-gray-600 text-lg">
               Discover the best services and experience the future with us.
             </p>
             <Link href="/listbarang">
-              <button className="mt-6 px-6 py-3 bg-[#B9E5E8] text-gray-800 font-semibold rounded-lg shadow-md hover:bg-teal-600">
+              <button className="mt-6 px-6 py-3 bg-[#B9E5E8] text-gray-800 font-semibold rounded-lg shadow-md hover:bg-teal-600 transition-all duration-300">
                 Lihat Barang
               </button>
             </Link>
           </div>
-          <div className="ml-24 mb-20 md:w-1/2 flex justify-center">
+
+          {/* Gambar Section */}
+          <div className="md:w-1/2 flex justify-end">
             <img
               src="/logo1.png"
               alt="Hero Illustration"
-              className="w-80 md:w-full"
+              className="w-full max-w-[500px] h-auto object-contain"
             />
           </div>
         </div>
