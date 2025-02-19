1c09df58fc5405302b297146e8492c1eb9616cbc
Putri Velani <putrivelani228@gmail.com>
2025-02-20 00:10:44 +0700

    .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,43 +2,29 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-[white] border-b border-gray-300 px-40">
+    <div className="navbar bg-gradient-to-r from-green-100 to-blue-100 border-b border-green-100 px-20">
       <div className="flex-1">
-        <h1 className="text-xl font-bold text-gray-800">
-          BEAR RENTAL
-        </h1>
+        <h1 className="text-xl font-bold text-gray-800">BEAR RENTAL</h1>
       </div>
       <div className="hidden lg:flex">
         <ul className="menu menu-horizontal px-1 text-gray-800">
-        <li>
-            <Link 
-              href="/" 
-              className="btn btn-ghost mr-4" 
-            >
+          <li>
+            <Link href="/" className="btn btn-ghost mr-4">
               Home
             </Link>
           </li>
           <li>
-            <Link 
-              href="/about" 
-              className="btn btn-ghost mr-4" 
-            >
+            <Link href="/about" className="btn btn-ghost mr-4">
               About
             </Link>
           </li>
           <li>
-            <Link 
-              href="/login" 
-              className="btn btn-ghost mr-4 bg-[#c0dee3]" 
-            >
+            <Link href="/login" className="btn btn-ghost mr-4 bg-green-200">
               Login
             </Link>
           </li>
           <li>
-            <Link 
-              href="/register" 
-              className="btn btn-ghost mr-4 bg-[#B9E5E8]"
-            >
+            <Link href="/register" className="btn btn-ghost mr-4 bg-blue-200">
               Sign In
             </Link>
           </li>
